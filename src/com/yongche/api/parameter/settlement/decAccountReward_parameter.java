03d0698fa5b264d6d3b3dee4030a5f1f79645da5
zhaoshiwei1 <447518646@qq.com>
2017-02-17 11:10:56 +0800

    support more interfaces

---
+++ src/com/yongche/api/parameter/settlement/decAccountReward_parameter.java
@@ -0,0 +1,59 @@
+package com.yongche.api.parameter.settlement;
+
+import com.yongche.api.parameter.parameter;
+import com.yongche.api.util.log;
+
+//接口名称 :处罚(两方分佣)
+
+public class decAccountReward_parameter extends parameter
+{
+	public String driver_id = "";
+	public String amount = "";
+	public String source = "";
+	public String rewards_type = "";
+	public String extra = "";
+	public String operator_id = "";
+	public String order_id = "";
+	
+	public decAccountReward_parameter()
+	{
+		
+	}
+	
+	@Override
+	public String get_url()
+	{
+		return null;
+	}
+	
+	@Override
+	public void set_parameter(String name, String value)
+	{
+		switch (name)
+		{
+			case "driver_id":
+				this.driver_id = value;
+				break;
+			case "amount":
+				this.amount = value;
+				break;
+			case "source":
+				this.source = value;
+				break;
+			case "rewards_type":
+				this.rewards_type = value;
+				break;
+			case "extra":
+				this.extra = value;
+				break;
+			case "operator_id":
+				this.operator_id = value;
+				break;
+			case "order_id":
+				this.order_id = value;
+				break;
+			default: 
+				log.out("参数体实例中未能发现对应的参数名", name);
+		}
+	}
+}
---
+++ src/com/yongche/api/parameter/settlement/incAccountAdjustAmount_parameter.java
@@ -0,0 +1,63 @@
+package com.yongche.api.parameter.settlement;
+
+import com.yongche.api.parameter.parameter;
+import com.yongche.api.util.log;
+
+//接口名称 :司机账户调增
+
+public class incAccountAdjustAmount_parameter extends parameter
+{
+	public String driver_id = "";
+	public String amount = "";
+	public String source = "";
+	public String rewards_type = "";
+	public String extra = "";
+	public String operator_id = "";
+	public String order_id = "";
+	public String realtime_income = "";//实时入账“true”, 否则不填
+	
+	public incAccountAdjustAmount_parameter()
+	{
+		
+	}
+	
+	@Override
+	public String get_url()
+	{
+		return null;
+	}
+	
+	@Override
+	public void set_parameter(String name, String value)
+	{
+		switch (name)
+		{
+			case "driver_id":
+				this.driver_id = value;
+				break;
+			case "amount":
+				this.amount = value;
+				break;
+			case "source":
+				this.source = value;
+				break;
+			case "rewards_type":
+				this.rewards_type = value;
+				break;
+			case "extra":
+				this.extra = value;
+				break;
+			case "operator_id":
+				this.operator_id = value;
+				break;
+			case "order_id":
+				this.order_id = value;
+				break;
+			case "realtime_income":
+				this.realtime_income = value;
+				break;
+			default: 
+				log.out("参数体实例中未能发现对应的参数名", name);
+		}
+	}
+}
